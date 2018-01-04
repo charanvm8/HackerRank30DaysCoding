3bce4488cef6f4d43aa09749fd01135d3f40611b
charanvm8 <charanvm8@gmail.com>
2018-01-03 23:25:31 -0500

    Adding new challenges

---
+++ src/challenges/TestClass.java
@@ -0,0 +1,23 @@
+package challenges;
+
+import java.util.Scanner;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Scanner sc = new Scanner(System.in);
+//		String str1="This is concatenated String";
+//		int i=sc.nextInt();
+//		double d=sc.nextDouble();
+//		sc.nextLine();
+//		String str=sc.nextLine();
+//		System.out.println(str+str1);
+		int i=20;
+		double k=(double)i/(double)100;
+		System.out.println(k);
+		
+
+	}
+
+}
